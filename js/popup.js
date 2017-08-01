c09db0a01e4aa2f05d4e83892bb0d263c1ba931c
robotgg <robotgg@126.com>
2017-08-01 11:16:25 +0800

    a

--- js/popup.js
+++ js/popup.js
@@ -2,3 +2,13 @@
 
 
 getAllTieba();
+
+$('#pkm').click(function () {
+    chrome.tabs.create({
+        url: 'https://github.com/pkmm',
+        active: true,
+    }, function (tab) {
+        console.log(tab);
+    });
+});
+
