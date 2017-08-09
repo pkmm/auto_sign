c9c3a10ba642ff796378f6650fa387f0edfee527
robotgg <robotgg@126.com>
2017-08-09 13:25:00 +0800

    update notification test

--- js/background.js
+++ js/background.js
@@ -1,11 +1,32 @@
 
 
-const period = 60 * 1; // 定时运行的脚本
+const PERIOD = 1 * 60; // 定时运行的脚本 单位 ：分钟
+const SIGN_TIEBA = 'sign_tieba_time_event';
+const NOTIFICATION_TEST = 'notification_test';
 
-chrome.alarms.create("time_at", {
+chrome.alarms.create(SIGN_TIEBA, {
     delayInMinutes:0,
-    periodInMinutes: period
+    periodInMinutes: PERIOD
+});
+chrome.alarms.create(NOTIFICATION_TEST, {
+    delayInMinutes:0,
+    periodInMinutes: 60,
 });
 chrome.alarms.onAlarm.addListener(function (ev) {
-    getAllTieba();
+    if(ev.name == SIGN_TIEBA) {
+        getAllTieba();
+    }
+    else if(ev.name == NOTIFICATION_TEST) {
+        chrome.notifications.create("id", opt, function(ev){
+            console.log(ev);
+        });
+    }
 });
+
+var opt = {
+  type: "basic",
+  title: "整点报时",
+  message: "现在时间是 "+(new Date()).getHours()+':00',
+  iconUrl: "../img/icon.png"
+}
+
